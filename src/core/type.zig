2f6c6e46a0b33c2ae00f241244c095099ae2e074
DavidMeagher1 <meagherjdavid@gmail.com>
2025-11-22 08:05:36 -0800

    feat(core): add WordIndex type definition for improved code clarity

--- src/core/type.zig
+++ src/core/type.zig
@@ -2,4 +2,5 @@ const Interpreter = @import("interpreter.zig");
 pub const Address = usize;
 pub const Cell = usize;
 pub const CodeIndex = usize;
+pub const WordIndex = usize;
 pub const Instruction = *const fn (interpreter: *Interpreter) anyerror!void;
