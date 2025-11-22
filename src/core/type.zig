e7fce64b605ea8f678e3c8a42059fa8f6062daa9
DavidMeagher1 <meagherjdavid@gmail.com>
2025-11-21 22:12:28 -0800

    feat(core): add core type definitions for Address, Cell, CodeIndex, and Instruction

---
+++ src/core/type.zig
@@ -0,0 +1,5 @@
+const Interpreter = @import("interpreter.zig");
+pub const Address = usize;
+pub const Cell = usize;
+pub const CodeIndex = usize;
+pub const Instruction = *const fn (interpreter: *Interpreter) anyerror!void;
